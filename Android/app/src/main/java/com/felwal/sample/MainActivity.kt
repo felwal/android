ad5ea2106277a431711b26cfe6214052b4cd4e5c
Felix Wallin <nils.felix@gmail.com>
2021-11-02 16:32:16 +0100

    Sample: btn fun

--- Android/app/src/main/java/com/felwal/sample/MainActivity.kt
+++ Android/app/src/main/java/com/felwal/sample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.felwal.sample
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.felwal.android.util.toast
 import com.felwal.android.widget.dialog.CheckDialog
@@ -8,25 +10,45 @@ import com.felwal.android.widget.dialog.ChipDialog
 import com.felwal.android.widget.dialog.SimpleDialog
 import com.felwal.android.widget.dialog.checkDialog
 import com.felwal.android.widget.dialog.chipDialog
+import com.felwal.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), CheckDialog.DialogListener, ChipDialog.DialogListener, SimpleDialog.DialogListener {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        toast("Hello World!")
+        btn("toast") {
+            toast("Hello World!")
+        }
 
-        checkDialog("Just checking in", "", arrayOf("Hej", "Då", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej",
-            "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej"
-        ), intArrayOf(1), tag = "tag")
-            .show(supportFragmentManager)
+        btn("check dialog") {
+            checkDialog("Just checking in", "", arrayOf("Hej", "Då", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej",
+                "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej"
+            ), intArrayOf(1), tag = "tag")
+                .show(supportFragmentManager)
+        }
 
-        chipDialog("Just checking in", "", arrayOf("Hej", "Då", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej",
-            "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej"
-        ), intArrayOf(1), tag = "tag")
-            .show(supportFragmentManager)
+        btn("chip dialog") {
+            chipDialog("Just checking in", "", arrayOf("Hej", "Då", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej",
+                "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej", "Hej"
+            ), intArrayOf(1), tag = "tag")
+                .show(supportFragmentManager)
+        }
+
     }
+
+    private fun btn(label: String = "", onClick: (View) -> Unit) {
+        val btn = Button(this)
+        btn.text = label
+        btn.setOnClickListener(onClick)
+        binding.root.addView(btn)
+    }
+
+    //
 
     override fun onCheckDialogPositiveClick(checkedItems: BooleanArray, tag: String) {
         toast(checkedItems.toString())
